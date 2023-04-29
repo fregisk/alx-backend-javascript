94e319d90173d2a92323ff7a33a6105383439d7f
fregisk <fregiskonan34@gmail.com>
2023-04-29 01:12:42 +0000

    tasks.

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,19 @@
+/**
+ * Retrieves the sum of ids of a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author YAO KOUASSI STÉPHANE <https://github.com/YAOSTEPHANE>
+ * @returns {Number}
+ */
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
